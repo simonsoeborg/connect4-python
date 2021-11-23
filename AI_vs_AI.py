8dc59866c3de4a65b01f3ab137b6ccffef047e7f
Kristoffer <s180500@win.dtu.dk>
2021-11-23 18:29:42 +0100

    tilføjet Ai vs Ai i Aibranch

---
+++ AI_vs_AI.py
@@ -0,0 +1,141 @@
+# Author: Keith Galli
+# Githun: https://github.com/KeithGalli/Connect4-Python
+
+import numpy as np
+import pygame
+import sys
+import math
+from connect4_controller import *
+from alpha_beta_algo_logic import *
+
+BLUE = (0, 102, 204)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+BLACK = (0, 0, 0)
+
+
+ROW_COUNT = 6
+COLUMN_COUNT = 7
+
+
+def create_board():
+    board = np.zeros((ROW_COUNT, COLUMN_COUNT))
+    return board
+
+
+def print_board(board):
+    print(np.flip(board, 0))
+
+
+def draw_board(board):
+    for c in range(COLUMN_COUNT):
+        for r in range(ROW_COUNT):
+            pygame.draw.rect(screen, BLUE, (c*SQUARESIZE, r *
+                                            SQUARESIZE+SQUARESIZE, SQUARESIZE, SQUARESIZE))
+            pygame.draw.circle(screen, WHITE, (int(
+                c*SQUARESIZE+SQUARESIZE/2), int(r*SQUARESIZE+SQUARESIZE+SQUARESIZE/2)), RADIUS)
+
+    for c in range(COLUMN_COUNT):
+        for r in range(ROW_COUNT):
+            if board[r][c] == 1:
+                pygame.draw.circle(screen, RED, (int(
+                    c*SQUARESIZE+SQUARESIZE/2), height-int(r*SQUARESIZE+SQUARESIZE/2)), RADIUS)
+            elif board[r][c] == 2:
+                pygame.draw.circle(screen, YELLOW, (int(
+                    c*SQUARESIZE+SQUARESIZE/2), height-int(r*SQUARESIZE+SQUARESIZE/2)), RADIUS)
+    pygame.display.update()
+
+
+board = create_board()
+print_board(board)
+game_over = False
+turn = 0
+
+pygame.init()
+
+SQUARESIZE = 100
+
+width = COLUMN_COUNT * SQUARESIZE
+height = (ROW_COUNT+1) * SQUARESIZE
+
+size = (width, height)
+
+RADIUS = int(SQUARESIZE/2 - 5)
+
+screen = pygame.display.set_mode(size)
+draw_board(board)
+pygame.display.update()
+
+myfont = pygame.font.SysFont("monospace", 75)
+
+turn = random.randint(0, 1)
+
+while not game_over:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+        # Ask for Player 1 Input
+        boolTurnAI = False
+        minmax_board = board.copy()
+        alpha = -math.inf
+        beta = math.inf
+        depth = 7
+
+        # vi vil gemme den column vi vil lægge i
+        # <---------------------------------------(Her kaldes algoritmen)
+        col, bestscore = alpha_beta_algo(
+            minmax_board, alpha, beta, boolTurnAI, depth)
+
+        row = get_next_open_row(board, col)
+        drop_piece(board, row, col, 1)
+
+        if winning_move(board, 1):
+            label = myfont.render("Player 1 wins", 1, RED)
+            screen.blit(label, (40, 10))
+            game_over = True
+
+        print_board(board)
+        draw_board(board)
+
+        turn += 1
+        turn = turn % 2
+
+        # # Ask for AI Input
+        if turn == 1 and not game_over:
+            # posx = event.pos[0]
+            # col = int(math.floor(posx/SQUARESIZE)
+            boolTurnAI = True
+            minmax_board = board.copy()
+            alpha = -math.inf
+            beta = math.inf
+            depth = 7
+
+            # vi vil gemme den column vi vil lægge i
+            # <---------------------------------------(Her kaldes algoritmen)
+            col, bestscore = alpha_beta_algo(
+                minmax_board, alpha, beta, boolTurnAI, depth)
+
+            row = get_next_open_row(board, col)
+            drop_piece(board, row, col, 2)
+
+            if winning_move(board, 2):
+                label = myfont.render("Player 2 wins", 1, YELLOW)
+                screen.blit(label, (40, 10))
+                game_over = True
+
+            print_board(board)
+            draw_board(board)
+
+            turn += 1
+            turn = turn % 2
+
+        if is_a_draw(board):
+            label = myfont.render("It's a draw", 1, BLACK)
+            screen.blit(label, (40, 10))
+            game_over = True
+
+        if game_over:
+            pygame.time.wait(6000)
